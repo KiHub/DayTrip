35644d83309ee8569885abbcce3d82d52e7c20b8
Kirill Ki <kirillkalapov@gmail.com>
2021-10-14 13:17:37 +0200

    First commit

--- DayTrip/ViewController.swift
+++ DayTrip/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    let placesNames = [
+        "Town Hall", "Saint Servatus", "Old Bridge", "Fort Sint Piter",
+        "City Hall", "Bonnefanten Museum", "Basilica", "Bishops Mill"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placesNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = placesNames[indexPath.row]
+        
+        
+        return cell!
+    }
 
 }
 
